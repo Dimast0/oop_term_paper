7553a339305ab525d9aa0928874131ab456ef722
Dimast0 <80773046+Dimast0@users.noreply.github.com>
2021-06-03 01:06:40 +0300

    FIles uploaded

---
+++ PawnshopApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLogic;
+
+namespace PawnshopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pawnshop OurPawnshop = new Pawnshop(10000);
+            ControlPanel.AddExampleData(OurPawnshop);
+            ControlPanel.Actions(OurPawnshop);
+        }
+    }
+}
+
